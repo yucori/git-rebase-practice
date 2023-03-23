5a56eee50be8b4d0164cdf9e8540f38a17ef3a8f
jang_jiwon <kooriangman@gmail.com>
2023-03-23 14:31:54 +0900

    [ADD] signup 기능 추가
    [MODIFY] signup 기능 수정

--- Backend/git-rebase-test/app.js
+++ Backend/git-rebase-test/app.js
@@ -44,3 +44,23 @@ app.listen(PORT, () => {
     });
   console.log(`Listening to request on localhost:${PORT}`);
 });
+
+app.post("/users/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  return await appDataSource.query(
+    `
+      INSERT INTO
+        users (
+          username,
+          email,
+          password			
+        )
+      VALUES (
+        ?,
+        ?,
+        ?
+      )
+    `,
+    [username, email, password]
+  );
+});
